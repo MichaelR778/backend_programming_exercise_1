01c950a25631165f4413605594e767e657f1b5c4
Michael Rahardja <michaelrahardja23@gmail.com>
2024-03-08 20:03:22 +0700

    535230115 Michael Rahardja

---
+++ soal_2.js
@@ -0,0 +1,35 @@
+const students = [
+    {
+        id: "535250001",
+        score: 98.0
+    },
+    {
+        id: "535250001",
+        score: 45.0
+    },
+    {
+        id: "535250001",
+        score: 97.75
+    },
+    {
+        id: "535250001",
+        score: 99.25
+    },
+    {
+        id: "535250001",
+        score: 78.5
+    },
+];
+
+students.sort((a, b) => {
+    const scoreA = a.score;
+    const scoreB = b.score;
+    if (scoreA > scoreB) {
+        return -1;
+    }
+    if (scoreA < scoreB) {
+        return 1;
+    }
+}); // FIXME
+
+console.log(students);
